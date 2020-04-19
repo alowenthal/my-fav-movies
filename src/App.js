3e50064222b71aab88b1d8ce945b9fe947185fe0
Adam Lowenthal <adam@lowenthalfamily.com>
2020-04-19 18:11:57 -0400

    Basic add/remove functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {});
+
+    function addMovie(query) {
+        fetch(`http://www.omdbapi.com/?t=${query}&apikey=bdf0ef4e`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const movieInfo = {
+                    title: data.Title,
+                    id: data.imdbID
+                };
+
+                setList((prevState) => [...prevState, movieInfo]);
+            });
+    }
+
+    function removeMovie(id) {
+        const removeIndex = myList
+            .map((item) => {
+                return item.id;
+            })
+            .indexOf(id);
+
+        // remove object
+        myList.splice(removeIndex, 1);
+
+        setList([...myList]);
+    }
+
+    const [query, setQuery] = useState("");
+    const [myList, setList] = useState([]);
+
+    return (
+        <div className="App">
+            <input
+                onChange={(event) => setQuery(event.target.value)}
+                name="username"
+                type="text"
+                placeholder="Enter a movie..."
+            />
+            <button onClick={() => addMovie(query)} type="button">
+                Add To List
+            </button>
+
+            <ul className="movies">
+                {myList.map((movie) => {
+                    return (
+                        <li className="movie" key={`movie--${movie.id}`}>
+                            {movie.title}
+                            <button
+                                onClick={() => removeMovie(movie.id)}
+                                type="button"
+                            >
+                                Remove
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
